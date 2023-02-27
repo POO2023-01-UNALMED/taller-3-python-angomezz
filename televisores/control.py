021a0b14d76779669045135c9ffb94f9bdec92f2
angomezz <angomezz@unal.edu.co>
2023-02-26 20:19:12 -0500

    Intento 1

---
+++ televisores/control.py
@@ -0,0 +1,25 @@
+class Control:
+    def __init__(self):
+        self.tv = None
+    
+    def turnOn(self):
+        self.tv.turnOn()
+    def turnOff(self):
+        self.tv.turnOff()
+    
+    def canalUp(self):
+        self.tv.canalUp()
+    def canalDown(self):
+        self.tv.canalDown()
+
+    def volumenUp(self):
+        self.tv.volumenUp()
+    def volumenDown(self):
+        self.tv.volumenDown()
+    
+    def setCanal(self, canal):
+        self.tv.setCanal(canal)
+    
+    def enlazar(self, tv):
+        self.tv = tv
+        tv.setControl(self)
